831dc84f5e97a819c699eb2a242f17f0d3e587f7
ItsukiTK <70346518+ItsukiTK@users.noreply.github.com>
2020-08-28 08:41:21 -0300

    Create spotify.js

---
+++ comandos/informativos/spotify.js
@@ -0,0 +1,58 @@
+  
+const Discord = require("discord.js");
+const convert = require("parse-ms");
+
+exports.run = async (client, message, args) => {
+    let user;
+    if (message.mentions.users.first()) {
+        user = message.mentions.users.first();
+    } else {
+        user = message.author;
+    }
+
+    let status;
+    if (user.presence.activities.length === 1) status = user.presence.activities[0];
+    else if (user.presence.activities.length > 1) status = user.presence.activities[1];
+
+    if (user.presence.activities.length === 0 || status.name !== "Spotify" && status.type !== "LISTENING") {
+       return message.channel.send({embed: {color: '#f17a19', description: "<:erro:746742674933022731> | Não foi encontrada nenhuma música em RichPresence do usuário."}})
+    }
+
+    if (status !== null && status.type === "LISTENING" && status.name === "Spotify" && status.assets !== null) {
+        let image = `https://i.scdn.co/image/${status.assets.largeImage.slice(8)}`,
+            url = `https:/open.spotify.com/track/${status.syncID}`,
+            name = status.details,
+            artist = status.state,
+            album = status.assets.largeText,
+            timeStart = status.timestamps.start,
+            timeEnd = status.timestamps.end,
+            timeConvert = convert(timeEnd - timeStart);
+
+        let minutes = timeConvert.minutes < 10 ? `0${timeConvert.minutes}` : timeConvert.minutes;
+        let seconds = timeConvert.seconds < 10 ? `0${timeConvert.seconds}` : timeConvert.seconds;
+        let time = `${minutes}:${seconds}`;
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Informação obtida do RichPresence do usuário.", "https://image.flaticon.com/icons/svg/2111/2111624.svg")
+        .setColor('#f17a19')
+        .setThumbnail(image)
+        .addField("Nome:", name, true)
+        .addField("Album:", album, true)
+        .addField("Artista:", artist, true)
+        .addField("Duração:", time, false)
+        .addField("Música sendo listada no spotify:", `[\`${artist} - ${name}\`](${url})`, false)
+        return message.channel.send(embed)
+    }
+}
+
+exports.help = {
+    name: "spotify",
+    description: "Show a Listening Spotify user status.",
+    usage: "/spotify [@user]",
+    example: "/spotify @ray#1337"
+  };
+  
+  exports.conf = {
+    aliases: [],
+    cooldown: 5
+  }
